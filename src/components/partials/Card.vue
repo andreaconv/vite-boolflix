704489b65bab962dfb40d5e006ffab5403792ac9
andreaconv <andrea2convertini@gmail.com>
2023-05-04 17:16:36 +0200

    [add] bozza visualizzazione lingue bandiere

--- src/components/partials/Card.vue
+++ src/components/partials/Card.vue
@@ -22,7 +22,9 @@ export default {
   <div class="card">
     <h1>{{ title }}</h1>
     <h2>{{ original_title }}</h2>
-    <h3>{{ original_language }}</h3>
+    <img v-if="original_language === 'en'" src="../../assets/en.png">
+    <img v-if="original_language === 'it'" src="../../assets/it.png">
+    <h3 v-if="original_language !== 'en' || original_language !== it">{{ original_language }}</h3>
     <h4>{{ vote_average }}</h4>
   </div>
 
@@ -31,5 +33,10 @@ export default {
 <style lang="scss" scoped>
 .card{
   color: white;
+  text-align: center;
+
+  img{
+    width: 40px;
+  }
 }
 </style>
