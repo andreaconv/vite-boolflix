d59caed87605bf48c5f0bdeaeb4b84228101cb5d
andreaconv <andrea2convertini@gmail.com>
2024-02-27 17:49:46 +0100

    [edit] cards

--- src/components/partials/Card.vue
+++ src/components/partials/Card.vue
@@ -4,11 +4,7 @@ import { store } from '../../data/store';
 export default {
   name: 'Card',
   props:{
-    image: String,
-    title: String,
-    original_title: String,
-    original_language: String,
-    vote_average: Number,
+    card: Object,
   },
   data() {
       return{
@@ -17,52 +13,34 @@ export default {
   },
   computed:{
     selectImg(){
-      return  `/src/assets/flags/language-${this.original_language}.svg`
+      return  `/flags/language-${this.card.original_language}.svg`
     }
   }
 }
 </script>
 
 
-<!-- <div v-if="store.popularMovie.length > 0" class="row movie-pop">
-  <swiper
-    :slides-per-view="5"
-    :space-between="50"
-    @swiper="onSwiper"
-    @slideChange="onSlideChange">
-    <swiper-slide v-for="card in store.popularMovie" 
-    :key="card.id">
-      <Card 
-        :image="card.poster_path"
-        :title="card.title"
-        :original_title="card.original_title" 
-        :original_language="card.original_language"
-        :vote_average="Math.round(card.vote_average / 2)"
-      />
-    </swiper-slide>
-</swiper>
-</div> -->
-
-
-
-
 <template>
   
   <div class="card">
-    <img class="poster" :src="`https://image.tmdb.org/t/p/w342/${image}`" alt="">
+    <img v-if="card.poster_path" class="poster" :src="`https://image.tmdb.org/t/p/w780/${card.poster_path}`" alt="">
+    <img v-else class="poster" src="/placeholder.jpg" alt="">
 
     <div class="info">
 
       <ul>
-        <li>Titolo: {{ title }}</li>
-        <li>Titolo originale: {{ original_title }}</li>
-        <li></li>
+        <li>Titolo: {{ card.title ? card.title : card.name }}</li>
+        <li>Titolo originale: {{ card.original_title ? card.original_title : card.original_name }}</li>
+        <li>Data di uscita: {{ card.release_date ? card.release_date : card.first_air_date}}</li>
+        <li>Lingua: <img class="flag" :src="selectImg" :alt="card.original_language"></li>
+        <li>
+          <div class="stars">
+            <span>Voti:</span>
+            <i v-for="star in Math.round(card.vote_average / 2)" :key="star" class="fa-solid fa-star"></i>
+            <i v-for="star in (5 - Math.round(card.vote_average / 2))" :key="star" class="fa-regular fa-star"></i>
+          </div>
+        </li>
       </ul>
-      <img class="flag" :src="selectImg" :alt="original_language">
-      <div class="stars">
-        <i v-for="star in vote_average" :key="star" class="fa-solid fa-star"></i>
-        <i v-for="star in (5 - vote_average)" :key="star" class="fa-regular fa-star"></i>
-      </div>
 
     </div>
 
@@ -71,34 +49,54 @@ export default {
 </template>
 
 <style lang="scss" scoped>
-.card{
-  height: 60vh;
-  margin: 0 0.5rem;
-  position: relative;
-  background-color: brown;
+  @use '../../scss/partials/vars' as *;
 
+.card{
+  position: relative;
+  
+  // TODO: overflow: hidden per togliere l'eccesso delle info e strecciare le immagini del poster per averle tutte alla stesa altezza
   img.poster{
-    height: 100%;
+    width: 100%;
+    max-width: 300px;
+    border-radius: 10px;
   }
-  &:hover .info{
-    display: block;
-  }
+  &:hover{
+    .info{
+      display: block;
+    }
+  } 
 
   .info{
     display: none;
+    animation: slideUp .8s ease;
+    border-radius: 10px;
     position: absolute;
     top: 0;
     width: 100%;
     height: 100%;
     background-color: rgba(0,0,0, 0.8);
-
+    padding: .6rem;
+    li{
+      padding-bottom: .4rem;
+    }
     .stars i{
     font-size: 1.2rem;
+    color: $tertiary-color;
     }
     img.flag{
       width: 40px;
     }
   }
+
+  
+@keyframes slideUp{
+  0% {
+    transform: translateY(+120%)
+  }
+  to {
+    transform: translateY(0)
+  }
+}
 
   
 }
